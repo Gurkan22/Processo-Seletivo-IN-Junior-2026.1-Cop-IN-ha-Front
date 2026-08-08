a0fbcab0ae7ad3276863fc6e39e4f58de89b32bf
Pedro Almeida <pe_almeida@id.uff.br>
2026-08-07 22:28:28 -0300

    fix: apiFetch

--- src/services/mockData.ts
+++ src/services/mockData.ts
@@ -20,7 +20,7 @@ async function apiFetch<T>(path: string, options: RequestInit = {}): Promise<T> 
     res = await fetch(`${API_URL}${path}`, {
       ...options,
       headers: {
-        'Content-Type': 'application/json',
+        ...(options.body ? { 'Content-Type': 'application/json' } : {}),
         ...(token ? { Authorization: `Bearer ${token}` } : {}),
         ...options.headers,
       },
